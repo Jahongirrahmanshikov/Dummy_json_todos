6818b051730ba2e40ab604765908b32eaf34fae6
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 10:18:57 +0500

    Get a random Todo

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'package:htt_get_post/models/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 void main([List<String>? args]) async {
-  Uri uri = Uri.parse("https://dummyjson.com/todos/1");
+  Uri uri = Uri.parse("https://dummyjson.com/todos/random");
   http.Response response = await http.get(uri);
 
   if (response.statusCode == 200) {
