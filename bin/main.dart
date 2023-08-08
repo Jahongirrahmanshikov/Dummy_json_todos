a77c47d2fdf439023e41a9d1eb93ac80621f6e98
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 10:19:57 +0500

    Limit and Skip todo

--- bin/main.dart
+++ bin/main.dart
@@ -3,11 +3,11 @@ import 'package:htt_get_post/models/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 void main([List<String>? args]) async {
-  Uri uri = Uri.parse("https://dummyjson.com/todos/random");
+  Uri uri = Uri.parse("https://dummyjson.com/todos?limit=3&skip=10");
   http.Response response = await http.get(uri);
 
   if (response.statusCode == 200) {
-    User todos = User.fromJson(jsonDecode(response.body));
+    TodoModel todos = TodoModel.fromJson(jsonDecode(response.body));
     print(todos);
   } else {
     print("Error ${response.statusCode}");
