e58b5ba23a7385dbd070b6edf82d3b954d1cc851
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 10:40:06 +0500

    Delete a todo

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,11 @@
-import 'dart:convert';
-
-import 'package:htt_get_post/models/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 void main([List<String>? args]) async {
-  Uri uri = Uri.parse("https://dummyjson.com/todos/12");
+  Uri uri = Uri.parse("https://dummyjson.com/todos/1");
 
-  http.Response response = await http.put(
-    uri,
-    body: jsonEncode({'todo': 'Flutter'}),
-    headers: <String, String>{
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  );
+  http.Response response = await http.delete(uri);
   if (response.statusCode == 200) {
-    print(response.body);
-    print("Update Successfully");
+    print("Deleted Successfully");
   } else {
     print("Error ${response.statusCode}");
   }
