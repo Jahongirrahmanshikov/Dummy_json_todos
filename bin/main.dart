79c2eba5fb9499f8dd8eee9e8e2a15ff70ee71a9
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 10:18:09 +0500

    Get a single todo

--- bin/main.dart
+++ bin/main.dart
@@ -3,11 +3,11 @@ import 'package:htt_get_post/models/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 void main([List<String>? args]) async {
-  Uri uri = Uri.parse("https://dummyjson.com/todos");
+  Uri uri = Uri.parse("https://dummyjson.com/todos/1");
   http.Response response = await http.get(uri);
 
   if (response.statusCode == 200) {
-    TodoModel todos = TodoModel.fromJson(jsonDecode(response.body));
+    User todos = User.fromJson(jsonDecode(response.body));
     print(todos);
   } else {
     print("Error ${response.statusCode}");
