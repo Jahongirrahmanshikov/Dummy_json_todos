70cb3d8ec8cdacc9d522570fd03444e2a3848f16
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 10:38:38 +0500

    Update a todo

--- bin/main.dart
+++ bin/main.dart
@@ -4,18 +4,18 @@ import 'package:htt_get_post/models/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 void main([List<String>? args]) async {
-  Uri uri = Uri.parse("https://dummyjson.com/todos/add");
+  Uri uri = Uri.parse("https://dummyjson.com/todos/12");
 
-  User user = User(id: 0, todo: "Hello world", completed: false, userId: 12);
-
-  http.Response response = await http.post(
+  http.Response response = await http.put(
     uri,
-    body: jsonEncode(user.toMap()),
-    headers: {"Content-Type": "application/json"},
+    body: jsonEncode({'todo': 'Flutter'}),
+    headers: <String, String>{
+      "Content-Type": "application/json; charset=UTF-8",
+    },
   );
-
   if (response.statusCode == 200) {
-    print("Addes Successfully");
+    print(response.body);
+    print("Update Successfully");
   } else {
     print("Error ${response.statusCode}");
   }
